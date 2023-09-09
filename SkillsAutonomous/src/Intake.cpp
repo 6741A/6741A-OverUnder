01654cd17e77d0c7b4a438ed0a21615052520ded
VoidDroidCD <nindriod7@gmail.com>
2023-09-09 14:48:57 -0400

    Added Intake and Wings Functionality

---
+++ SkillsAutonomous/src/Intake.cpp
@@ -0,0 +1,41 @@
+#include "vex.h"
+#include "Intake.h"
+
+using namespace vex;
+
+void Intake::EnableIntake()
+{
+
+  IntakeMotor.spin(forward, 100, percent);
+
+}
+
+void Intake::ReverseIntake()
+{
+
+  IntakeMotor.spin(reverse, 100, percent);
+
+}
+
+void Intake::DisableIntake()
+{
+
+  IntakeMotor.stop();
+
+}
+
+void Intake::ExtendIntake()
+{
+
+  IntakePistonLeft = true;
+  IntakePistonRight = true;
+  
+}
+
+void Intake::RetractIntake()
+{
+
+  IntakePistonLeft = false;
+  IntakePistonRight = false;
+  
+}
--- SkillsAutonomous/src/Robot.cpp
+++ SkillsAutonomous/src/Robot.cpp
@@ -1,6 +1,7 @@
 #include "vex.h"
 #include "Robot.h"
 #include "Catapult.h"
+#include "Intake.h"
 #include "Base.h"
 
 using namespace vex;
@@ -32,4 +33,32 @@ void Robot::DriveForward(float targetDistance, float roomForError)
 void Robot::GoTo(float targetX, float targetY, float rotationError, float distanceError)
 { base.GoTo(targetX, targetY, rotationError, distanceError); }
 
+// Intake Functionality
+
+Intake intake;
+
+void Robot::EnableIntake()
+{ intake.EnableIntake(); }
+
+void Robot::ReverseIntake()
+{ intake.ReverseIntake(); }
+
+void Robot::DisableIntake()
+{ intake.DisableIntake(); }
+
+void Robot::ExtendIntake()
+{ intake.ExtendIntake(); }
+
+void Robot::RetractIntake()
+{ intake.RetractIntake(); }
+
+// Wings Functionality
+
+Wings wings;
+
+void Robot::ExtendWings()
+{ wings.ExtendWings(); }
+
+void Robot::RetractWings()
+{ wings.RetractWings(); }
 
