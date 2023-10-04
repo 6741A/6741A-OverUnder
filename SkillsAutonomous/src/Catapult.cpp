2b6e90c0cd03ed2c886ef85ebecfa8927c64d8f1
Cole <nindriod7@gmail.com>
2023-10-04 16:17:37 -0400

    Changed autoh stuff

--- SkillsAutonomous/src/Catapult.cpp
+++ SkillsAutonomous/src/Catapult.cpp
@@ -21,20 +21,28 @@ void Catapult::StartCatapultMatchLoading()
 {
 
   int launches = 0;
+  bool added = false;
 
-  while (launches < 44)
+  while (launches < 43)
   {
+    Controller1.Screen.clearScreen();
+    Controller1.Screen.setCursor(0,0);
+    Controller1.Screen.print(launches);
 
     CatapultMotor.spin(forward, 100, percent);
 
-    if (Rotation9.position(degrees) > 290)
+    if ((Rotation9.angle() > 62) && (added == false))
     {
-      Controller1.Screen.clearScreen();
-      Controller1.Screen.setCursor(0,0);
-      Controller1.Screen.print(launches);
-      CatapultMotor.spin(forward, 100, percent);
-      waitUntil(!(Rotation9.position(degrees) > 290));
+
       launches++;
+      added = true;
+      //waitUntil(!(Rotation9.angle() > 62));
+      
+    }
+
+    if (Rotation9.angle() < 62)
+    {
+      added = false;
     }
 
     if (launches == 40)
